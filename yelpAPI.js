6102815a7dab8f989daf07d64b27819579c8d912
kferny1 <kferny1@gmail.com>
2018-01-20 11:09:28 -0700

    yelp api

--- yelpAPI.js
+++ yelpAPI.js
@@ -1,46 +1,38 @@
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.yelp.com/v3/businesses/search?term=restaurant&latitude=40.82783908257346&longitude=-74.10162448883057",
-    "method": "GET",
-    "headers": {
-      "authorization": "Bearer aPZGg2PjfUOmvrqGB-6y-FCO11Et4w0_04R7_XZm-LTkwmClkSebFC4-dmSaExcw4EyyoD05iU2yWTtfj8YS30k-2bq_ncc1yuVzPinhX5Sm8AfhJFanOTmdowpgWnYx",
-      "cache-control": "no-cache",
-    }
-  }
-  
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+var auth = { 
+    clientId: "xtf13fCaEfd5YZdqkQaTPA", 
+    apiKey: "aPZGg2PjfUOmvrqGB-6y-FCO11Et4w0_04R7_XZm-LTkwmClkSebFC4-dmSaExcw4EyyoD05iU2yWTtfj8YS30k-2bq_ncc1yuVzPinhX5Sm8AfhJFanOTmdowpgWnYx",
+    clientSecret: "3EbQxNbJvqFGpqKosuIZtLSTbxr9Vg5BetCGgwMa3WgfLc21snc0M0yXMxVxFVJw",
+    
+  };
 
+  var terms = '';
+  var near = 'saltLakeCity';
 
+  var accessor = {
+    consumerSecret: auth.clientSecret,
+  };
 
-fetch('https://api.yelp.com/oauth2/token?client_id=<CLIENT_ID>&client_secret=<CLIENT_SECRET>', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  body: JSON.stringify({
-    grant_type: 'client_credentials',
-  })
-})
-  .then((res) => res.json())
-  .then((resJSON) => {
-    this.setState({ access_token: resJSON.access_token });
-    console.log(resJSON)
-  })
-  .then(() => {
-    fetch('https://api.yelp.com/v3/businesses/search?location=12345', {
-      method: 'GET',
-      headers: {
-        'authorization': 'Bearer ' + this.state.access_token
-      }
-    })
-    .then((res) => res.json())
-    .then((resJSON) => {
-      console.log('resJSON', resJSON)
-    })
-  })
-  .catch((err) => {
-    console.log('err', err);
-  })
+  var parameters = [];
+  parameters.push(['term', terms]);
+  parameters.push(['location', near]);
+  parameters.push(['oauth_client_id', auth.clientId]);
+  parameters.push(['oauth_api_key', auth.apiKey]);
+  parameters.push(['oauth_client_secret', auth.clientSecret]);
+
+  var message = { 
+    'action': 'http://api.yelp.com/v2/search',
+    'method': 'GET',
+    'parameters': parameters 
+  };
+
+  OAuth.setTimestampAndNonce(message);  
+
+  OAuth.SignatureMethod.sign(message, accessor);
+
+  var parameterMap = OAuth.getParameterMap(message.parameters);
+  parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+
+  var url = OAuth.addToURL(message.action,parameterMap);
+  var response = UrlFetchApp.fetch(url).getContentText();
+  var responseObject = Utilities.jsonParse(response);
+  //have my JSON object, do whatever we want here, like add to spreadsheets
