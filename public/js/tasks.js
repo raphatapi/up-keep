90ba5d33b628afead8ecccc1ec7d18db20018d4a
raphatapi <raphaeltapioca10@gmail.com>
2018-01-21 17:06:59 -0700

    select to modal

--- public/js/tasks.js
+++
@@ -1,8 +0,0 @@
-import filterTask from './filterTask'
-
-export function handleFilterTasksClick() {
-    console.log("here?");
-    var list = [1, 2, 3];
-    var filteredList = filterTask(blah, blah, list);
-    // filterTask('data-category', 'HVAC');
-}
