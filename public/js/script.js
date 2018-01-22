205c9924639ce05907a493999d3c55c2ec9bede2
raphatapi <raphaeltapioca10@gmail.com>
2018-01-22 15:54:27 -0700

    moved print button script to js folder

--- public/js/script.js
+++ public/js/script.js
@@ -30,6 +30,26 @@ $(document).ready(function(){
       $('#myModal').on('hidden.bs.modal', function () {
        location.reload();
       })
+
+      document.getElementById("btnPrint").onclick = function () {
+        printElement(document.getElementById("printThis"));
+    }
+    
+    function printElement(elem) {
+        var domClone = elem.cloneNode(true);
+        
+        var $printSection = document.getElementById("printSection");
+        
+        if (!$printSection) {
+            var $printSection = document.createElement("div");
+            $printSection.id = "printSection";
+            document.body.appendChild($printSection);
+        }
+        
+        $printSection.innerHTML = "";
+        $printSection.appendChild(domClone);
+        window.print();
+    }
     
  });
 
