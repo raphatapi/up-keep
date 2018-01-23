f3c1cc511c4684c7c0138fa0020ec5f1f7d3ec69
Brady <brady.withers@outlook.com>
2018-01-23 10:39:45 -0700

    misc css

--- tests/bradytest.js
+++ tests/bradytest.js
@@ -6,10 +6,7 @@ nightmare
   .goto("http://up-keeps.herokuapp.com/upkeep/howto")
   .type("#youtubeSearch", "anode rod replacement")
   .click("#searchButton")
-  .wait("#links a")
-  .evaluate(function() {
-    return document.querySelector("#links a").href;
-  })
+  
   .end()
   .then(function(result) {
     console.log(result);
