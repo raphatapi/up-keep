14def82c402815d1ff7345ea52809cf0ba2fef69
Brady <brady.withers@outlook.com>
2018-01-23 11:03:03 -0700

    css shadows

--- tests/bradytest.js
+++ tests/bradytest.js
@@ -6,7 +6,10 @@ nightmare
   .goto("http://up-keeps.herokuapp.com/upkeep/howto")
   .type("#youtubeSearch", "anode rod replacement")
   .click("#searchButton")
-  
+  .wait("#links a")
+  .evaluate(function() {
+    return document.querySelector("#links a").href;
+  })
   .end()
   .then(function(result) {
     console.log(result);
