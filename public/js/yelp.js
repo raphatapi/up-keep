b9b9596dbbdf1a30b234e281cec19b2677b2bc99
kferny1 <kferny1@gmail.com>
2018-01-24 16:05:05 -0700

    yelp working

--- public/js/yelp.js
+++ public/js/yelp.js
@@ -1,29 +1,87 @@
- window.onload = function(){
+window.onload = function(){
+  var $yelp = $("#yelp");
+  var $location = $("#yelpLocation");
 
-        $("#searchButton").on("click", function(){
-            $("#yelp").empty();
-            var yelpSearch1 = document.getElementById("yelpTerm").value;
-            var yelpSearch2 = document.getElementById("yelpLocation").value;
-            var yelpSearch = "yelpSearch1" + "yelpSearch2";
-            console.log("hello");
-                
-            // do a GET to your Yelp endpoint in your server
-            $.get("/api/yelp/"+yelpSearch).done(function(result){
-                var searchResults = result.items;
-                for(var i=0; i < 5; i++) {
-                    var name = searchResults[i].name;
-                    var image = searchResults[i].image_url;
-                    var URL = searchResults[i].url;
-                    var rating = searchResults[i].rating;
-            
-                    $("#yelp").append(`<div class="how to">
-                        <img src = "${image}">
-                        ${name}
-                        ${URL}
-                        ${rating}
-                    </div>`
-                    );
-                };
-            });
-        });
-    };
+ $("#yelpSearchBttn").on("click", function(){
+  var searchTerm = document.getElementById("yelpTerm").value;
+  var locationInput = document.getElementById("yelpLocation").value;
+  const sort_by = 'rating';
+  const limit = '5';
+  searchStuff(locationInput, searchTerm, limit, sort_by);
+});
+
+function formatNumber(phoneNumber) {
+  phoneNumber = phoneNumber || "";
+  return phoneNumber.replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3");
+}
+
+function searchStuff(queryLocation, queryTerm, queryLimit, querySort) {
+  $yelp.empty();
+  // If location input is empty, show an error
+  if ($location.val() == '') {
+    alert("Please add a location.");
+  // Else run specificed search
+  } else {
+      var query = $.param({
+        location: queryLocation,
+        term: queryTerm,
+        limit: queryLimit,
+        sort_by: querySort
+      });
+
+      var url = '/api/yelpSearch/?' + query
+      $.ajax({
+        url: url,
+        success: function(response){
+          var helpMeData = response.businesses
+          var helpMeQueryParams = [];
+
+          for (i = 0; i < 5; i++) {
+            var phoneNumber = helpMeData[i].phone;
+            var phone = formatNumber(phoneNumber);
+            var title = helpMeData[i].name;
+            var id = helpMeData[i].id;
+            var address = helpMeData[i].location.display_address[0];
+            var rating = helpMeData[i].rating;
+            var image = helpMeData[i].image_url;
+            var link = helpMeData[i].url;
+            // var output = '<img src=' +image + '>';
+            // $yelp.append(output);
+            $yelp.append(`<article class="help-me" id="${id}"><section class="featured-image"><a class="help-me-link" href="${link}" target="_blank"><img src="${image}" alt="" /></a></section><section class="help-me-content"><a class="help-me-link" href="${link}" target="_blank"><h3 class="title">${title}</h3></a><p class="address">${address} | <span class="phone">${phone}</span></p></section><section class="rating">Rating: ${rating}</section><div class="clearfix"></div></article>`);
+          };
+        },
+        error: function () {
+          console.log("Can't load because of error.");
+        }
+      })
+    }
+  }
+}
+// // Old version
+//         $("#searchButton").on("click", function(){
+//             $("#yelp").empty();
+//             var yelpSearch1 = document.getElementById("yelpTerm").value;
+//             var yelpSearch2 = document.getElementById("yelpLocation").value;
+//             var yelpSearch = "yelpSearch1" + "yelpSearch2";
+//             console.log("hello");
+//
+//             // do a GET to your Yelp endpoint in your server
+//             $.get("/api/yelp/"+yelpSearch).done(function(result){
+//                 var searchResults = result.businesses;
+//                 for(var i=0; i < 5; i++) {
+//                     var name = searchResults[i].name;
+//                     var image = searchResults[i].image_url;
+//                     var URL = searchResults[i].url;
+//                     var rating = searchResults[i].rating;
+//
+//                     $("#yelp").append(`<div class="how to">
+//                         <img src = "${image}">
+//                         ${name}
+//                         ${URL}
+//                         ${rating}
+//                     </div>`
+//                     );
+//                 };
+//             });
+//         });
+//     };
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var methodOverride = require("method-override");
 
+// Yelp API
+require('dotenv').config();
+var fs = require('fs');
+var path = require('path');
+const Yelp = require("./apis/yelpClass");
+const yelp = new Yelp({ apiKey: process.env.YELP_API_KEY });
+
+
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -16,7 +24,34 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/tasks_controller.js");
 
+// Addl Yelp change
+app.get('/api/yelpSearch', function (req, res){
+  var term = req.query.term;
+  var location = req.query.location;
+  var sort_by = 'rating';
+  var limit = '5';
+
+if (!term || !location) {
+  res.json({
+    error: 'You must include a term and a location!'
+  });
+}
+
+yelp.search({ term, location, sort_by, limit })
+  .then(data => {
+    return res.json(data);
+  })
+  .catch((e) => {
+    console.error("Error", e);
+  });
+});
+
 app.use("/", routes);
+
+// uncomment below for testing of yelp
+// app.listen(port, function(){
+//   console.log('The app is listening on port ' + port);
+// });
 
 db.sequelize.sync({}).then(function(){
   app.listen(port, function(){
